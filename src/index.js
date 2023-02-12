6e61f27d8117db98f1912f37dea99eefc8e2f766
GruntovGD <gruntovgd@gmail.com>
2023-02-13 00:36:17 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+  const numbs=['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen',]
+  const dec=['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+  return (number===0) ? 'zero' :
+  (number<20) ? numbs[number] :
+  (number<100) ? dec[+number.toString()[0]].concat(' ',numbs[+number.toString()[1]]).trim() :
+  (number<120) ? numbs[+number.toString()[0]].concat(' hundred ',numbs[+number.toString().slice(1)]).trim() :
+  numbs[+number.toString()[0]].concat(' hundred ',(number.toString().slice(1)!=0) ? toReadable(+number.toString().slice(1)) : '').trim()
 }
